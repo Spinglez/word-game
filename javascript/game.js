92cc38907b474f5b3b874f906020f16d877bc8cc
Spinglez <42229372+Spinglez@users.noreply.github.com>
2018-10-02 23:52:42 -0700

    added some new html to round out the game space and got the 'guess box' working in javascript TODO: track current number of letters left and display incorrect guesses to user

--- javascript/game.js
+++ javascript/game.js
@@ -13,45 +13,70 @@
 //  display progress (number of guesses remaining and correct amount guessed)
 //
 
+
 // defining words available
   let word1 = "we rise";
   let word2 = "show me";
   let word3 = "voices in my head";
   let word4 = "light";
   let word5 = "byu";
+  let word6 = "A light tes"
+  let word7 = "randomch"
+
 
 // array of available choices
-  var words = [word1, word2, word2, word4, word5];
+  var words = [word1, word2, word3, word4, word5, word6, word7];
 // end arrray
 
 // pulling a random word from the array above
-  var word = words[Math.floor(Math.ramdom() * words.length)];
+  var word = words[Math.floor(Math.random() * words.length)];
 
+  var targetDiv1 = document.getElementById('gameDiv');
+  var targetDiv2 = document.getElementById('scoreBox')
 // making the chosen word from the random choice above display as _ (underscores)
   var answerArray = [];
   for (var i = 0; i < word.length; i++){
-    answerArray[i] = "_";
+    answerArray[i] = " _";
+    // targetDiv.appendChild(answerArray[i]);
+    // console.log(answerArray[i]);
+  }
+  gameDiv.innerHTML = answerArray;
+
+  for (var j = 0; i < word.length; i++){
+    targetdiv2.a
   }
 
-  var lettersLeft = word.length;
+  //
+  // for (i = 0; i < answerArray.length; i++){
+  //   var newDiv = document.createElement("div");
+  //   newDiv.innerHTML
+  //    = answerArray[i];
+  // }
 
-  var playerGuess =
+  // targetDiv.documentWrite(answerArray[i]);
+  //
+  // var lettersLeft = word.length;
 
-  getElementById(('wordGame')answerArray.join(" "));
+  // var playerGuess =
+
+// printing the chosen word into the targeted
+  // var displayDiv getElementById('gameDiv');
+  //
+  //   displayDiv.textContent = answerArray.join(" ");
 
 // begin objects for array (now thinking objects is too much)
-
-  var word1 {
-    word: "we rise",
-    length: 7,
-  }
-
-  var word2 {
-    word: "show me",
-    length: 7,
-  }
-
-  var word3 {
-    word: "voices in my head",
-    length: 17,
-  }
+  //
+  // var word1 {
+  //   word: "we rise",
+  //   length: 7,
+  // }
+  //
+  // var word2 {
+  //   word: "show me",
+  //   length: 7,
+  // }
+  //
+  // var word3 {
+  //   word: "voices in my head",
+  //   length: 17,
+  // }
